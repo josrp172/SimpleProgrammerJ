9f351e3b2753c687b086e86ecebb7cf9f25ad64a
josrp172 <josrp172@gmail.com>
2016-01-15 20:53:03 -0800

    Update and rename ArrayListProgram to ArrayListProgram.java

--- ArrayListProgram.java
+++ ArrayListProgram.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
-public class array{
+public class ArrayListProgram{
     private static ArrayList name = new ArrayList();
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
